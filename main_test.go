6399f093cfabebf85e182dd2af4976a1069ddf39
LindSuen <lindsuen@foxmail.com>
2023-11-11 11:09:38 +0800

    test: add benchmark testing main_test.go

---
+++ main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/lindsuen0/mkpasswd/src"
+)
+
+const (
+	N uint64 = 999999
+	l uint64 = 16
+	n uint64 = 4
+	c uint64 = 4
+)
+
+func BenchmarkMain(b *testing.B) {
+	var i uint64
+	for i = 0; i < N; i++ {
+		src.RandomString(l, n, c)
+	}
+}
